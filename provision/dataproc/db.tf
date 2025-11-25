3f313788501b69945c9adb88ef85cff5b157222f
Lívia Lelis <git@lelis.dev>
2025-11-24 23:19:12 -0300

    feat: alloy db

--- provision/dataproc/db.tf
+++ provision/dataproc/db.tf
@@ -1,3 +1,12 @@
 data "google_sql_database_instance" "main" {
   name = "usp-olap-wildfire"
 }
+
+data "google_alloydb_cluster" "alloy" {
+  cluster_id = "usp-olap-wildfire-alloy"
+}
+
+data "google_alloydb_instance" "primary" {
+  cluster_id = data.google_alloydb_cluster.alloy.cluster_id
+  instance_id = "primary"
+}
